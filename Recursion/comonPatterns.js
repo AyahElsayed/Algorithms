5c774cd7a4c087ecaea3745bacec12ff7340da6d
Ayah <eng.aya.elsayed.2020@gmail.com>
2021-07-03 13:16:18 +0200

    Accumulators

--- Recursion/comonPatterns.js
+++ Recursion/comonPatterns.js
@@ -38,3 +38,27 @@ output:
     looping from 5 until 6
     looping from 6 until 6
 */
+
+// 2- ACCUMULATORS
+
+function joinElements(array, joinString) { // s , e
+
+    function recurse(index, resultSoFar) { // 0 , ""  ||| 1 , s
+        resultSoFar += array[index];  // 's'  ||| 'e'
+
+        if (index === array.length - 1) { // base case
+            return resultSoFar;
+        } else {
+            return recurse(index + 1, resultSoFar + joinString); // 1 , s ||| 2 , se
+        }
+    }
+
+    return recurse(0, '');
+}
+
+joinElements(['s', 'cr', 't cod', ' :) :)'], 'e');
+/*
+output
+    secret code :) :)
+*/
+
